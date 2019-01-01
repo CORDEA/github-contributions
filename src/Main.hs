0e4d273d47df04ec7e1c935c79867dc465cc5bd3
Yoshihiro Tanaka <contact@cordea.jp>
2019-01-01 11:54:26 +0900

    fetch contributions

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,30 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Network.HTTP.Conduit
+import Network.HTTP.Types.Header
+import Network.HTTP.Base ( defaultUserAgent )
+import Network.URI ( parseURI )
+import Data.ByteString.Lazy  ( ByteString )
+import Data.Text.Lazy
+import Data.Text.Lazy.Encoding
+
+url :: String
+url = "https://github.com/users/CORDEA/contributions"
+
+sendRequest :: IO ( Response ByteString )
+sendRequest =
+    newManager tlsManagerSettings >>= \m ->
+        httpLbs req { method = "GET" } m
+    where
+        Just req = parseUrlThrow url
+
+fetched :: Response ByteString -> IO ()
+fetched response =
+    putStrLn $ unpack $ decodeUtf8 resp
+    where
+        resp = responseBody response
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = fetched =<< sendRequest

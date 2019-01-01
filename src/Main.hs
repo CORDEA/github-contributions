6c98059847c44427b6b8f53d9354da35714f26e1
Yoshihiro Tanaka <contact@cordea.jp>
2019-01-01 16:18:43 +0900

    parse html

--- src/Main.hs
+++ src/Main.hs
@@ -7,8 +7,10 @@ import Network.HTTP.Types.Header
 import Network.HTTP.Base ( defaultUserAgent )
 import Network.URI ( parseURI )
 import Data.ByteString.Lazy  ( ByteString )
-import Data.Text.Lazy
 import Data.Text.Lazy.Encoding
+import Text.HTML.TagSoup
+import qualified Data.Text.Lazy as T
+import qualified Parser
 
 url :: String
 url = "https://github.com/users/CORDEA/contributions"
@@ -22,9 +24,11 @@ sendRequest =
 
 fetched :: Response ByteString -> IO ()
 fetched response =
-    putStrLn $ unpack $ decodeUtf8 resp
+    putStrLn $ unwords parsed
     where
         resp = responseBody response
+        tags = parseTags $ T.unpack $ decodeUtf8 resp
+        parsed = Parser.parse tags
 
 main :: IO ()
 main = fetched =<< sendRequest
